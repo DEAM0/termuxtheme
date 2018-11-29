02a35a0a997495bc534fdb50f07a35a122404909
rukit <45461292+rukit@users.noreply.github.com>
2018-11-29 16:11:02 +0100

    Auto Installer Template for termux

---
+++ script.sh
@@ -0,0 +1,9 @@
+pkg install figlet
+figlet Rukit INC
+pkg install git
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+pkg install sh
+cd termux-ohmyzsh
+sh install.sh
+echo "Thanks You Used Rukit Installer!"
+echo "Please Restart Termux App For Apply All Change!"
